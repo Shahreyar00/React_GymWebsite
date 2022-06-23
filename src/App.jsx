e1005fbec1e8a2d2ff0f53ef0b8dd0b6580920e0
Shahreyar Arif <70688937+Shahreyar00@users.noreply.github.com>
2022-06-23 14:30:35 +0530

    addedRedirect

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/exercise/:id" element={<ExerciseDetail />} />
+                    <Route path="*" element={<Home />} />
                 </Routes>
                 <Footer />
             </div>
